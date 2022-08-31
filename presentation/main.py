6492eb9aef18b31c2b289f3d772b1f0d35279ea6
theovald <gjoegjoe@outlook.com>
2022-08-31 20:24:40 +0200

    Docs

---
+++ presentation/main.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    from utils.defaults import defaults
+    from utils.front_page import front_page
+    from utils.kantega import kantega_footer
+
+    from presentation.utils.front_page import front_page
+
+    defaults()
+    front_page()
+    kantega_footer()
