240243d009f8bece148c446f75c39a99a1990557
theovald <gjoegjoe@outlook.com>
2022-09-02 16:17:08 +0200

    Started exers

--- presentation/pages/1_overview.py
+++ presentation/pages/1_overview.py
@@ -14,6 +14,9 @@ with tab0:
 
     if all([word in hva_gjor.lower() for word in ["hva", "gjør", "data", "scientist"]]):
         st.image("resources/image25.png", width=1500)
+    
+    elif all([word in hva_gjor.lower() for word in ["bør", "data", "scientist", "kunne"]]):
+        st.image("resources/image23.png", width=1500)
 
 
 with tab1:
--- presentation/utils/kantega.py
+++ presentation/utils/kantega.py
@@ -12,22 +12,28 @@ def kantega_footer():
             st.markdown(
                 """
             #### Vi som jobber i Kantega mener vi har verdens beste arbeidsplass. 
-            Vi eier alle like mye og vi jobber alle mot et felles mål. 
-            
-            I tillegg får vi jobbe med fantastiske kunder og viktige prosjekter som betyr mye for mange.
-            
+
             """
             )
+            st.video("https://www.youtube.com/watch?v=B4PWHtlukMU")
 
         with col2:
             st.markdown("### AI-teamet")
             st.image("resources/image66.png", width=450)
+            st.markdown(""" 
+            En fantastisk gjeng som har samlet seg om et felles mål:
+
+            ### Å levere ansvarlige og bærekraftige maskinlæringsløsninger som skaper verdi
+            """)
 
         with col3:
             st.markdown("### Nora Gjøen-Gjøsæter")
-            st.markdown("Data Scientist ... ")
+            st.markdown("Data Scientist og medeier")
             st.image(
                 "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSL6uEj3tSYgKCJXpHn-A9UuJiKFRPR5c9ZbrHBtbQWdlXey-_bNBpE8s97lgDEpjc2",
                 width=300,
             )
-            st.markdown("nora@kantega.no")
+            st.markdown("...og tidligere fotballspiller")
+        
+
+        st.markdown("#### *Vi eier alle like mye og vi jobber alle mot et felles mål. I tillegg får vi jobbe med fantastiske kunder og viktige prosjekter som betyr mye for mange.* ")
