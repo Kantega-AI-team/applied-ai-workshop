fce5dd9c9e18e6667f74df955696358c7e03580e
emanuele.gramuglia@kantega.no <emanuele.gramuglia@kantega.no>
2022-11-02 09:05:32 +0000

    change column format to upper case

--- notebooks/end_to_end_ML/data_extraction.py
+++ notebooks/end_to_end_ML/data_extraction.py
@@ -30,5 +30,8 @@ original_dataset_pd = original_dataset.toPandas()
 # drop _c0 column
 original_dataset_pd = original_dataset_pd.drop(["_c0"], axis = 1)
 
+# change column names from lower to upper case (so we can re-transform them in lower case during the silvering phase)
+original_dataset_pd = original_dataset_pd.rename(columns = str.upper)
+
 # store the pandas version of the original dataset in FileStore/raw_data
 original_dataset_pd.to_csv("/dbfs/FileStore/raw_data/naturkampen_original.csv", index = False)
